790d38fcc79f106058cf9050e86fc6cdd8812aa9
Liamm <78539920+daydaya@users.noreply.github.com>
2024-06-05 10:38:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://og.liamm.top', // The link to generate OG image, don't end with a slash
   socialLink: 'https://x.com/Liamm_top',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
