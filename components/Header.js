cbb682635c6138d6bf430a1cef40d5ecf00d41c0
Liamm <78539920+daydaya@users.noreply.github.com>
2024-06-05 12:49:53 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,10 +9,10 @@ const NavBar = () => {
   const BLOG = useConfig()
   const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: false, external: true },
-    { id: 3, name: 'YouTube', to: 'https://youtube.com/@liamm_top?sub_confirmation=1', show: true, external: true },
+    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: false },
+    { id: 1, name: locale.NAV.RSS, to: '/feed', show: false, external: true },
+    { id: 2, name: 'YouTube', to: 'https://youtube.com/@liamm_top?sub_confirmation=1', show: true, external: true },
+    { id: 3, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
